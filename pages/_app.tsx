5bc77e7db452fe6eafb62eade6a8b27f20c3d5bb
Ben Brackenbury <benbrack22@icloud.com>
2022-07-16 19:38:40 +0100

    added lang attribute to html tag and meta description

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,14 @@
 import '../styles/index.scss'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import { Footer } from '../layouts'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div className='app'>
+      <Head>
+        <meta name="description" content="Ben Brackenbury, web, iOS, and macOS developer." />
+      </Head>
       <Component {...pageProps} />
       <Footer/>
     </div>
