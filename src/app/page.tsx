cc7473fd52688cefaad3b287e1943556ea633b10
Ben Brackenbury <benbrack22@outlook.com>
2023-08-20 21:56:05 +0100

    changed highlight colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,57 @@
-import { IconDefinition, faAppStore, faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  IconDefinition,
+  faAppStore,
+  faGithub,
+  faLinkedinIn,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Home() {
   return (
-    <main className="grid place-items-center w-[100dvw] h-[100dvh] px-6">
-      <div className="flex flex-col justify-center align-middle text-center gap-4 w-full">
-        <h1 className="text-5xl sm:text-6xl font-bold">Ben Brackenbury</h1>
+    <main className="grid h-[100dvh] w-[100dvw] place-items-center px-6">
+      <div className="flex w-full flex-col justify-center gap-4 text-center align-middle">
+        <h1 className="text-5xl font-bold sm:text-6xl">Ben Brackenbury</h1>
         <p className="text-2xl">Web & iOS Developer</p>
 
-        <div className="flex flex-row justify-center gap-7 text-4xl my-5 select-none mb-20">
-          <SocialLink href='https://x.com/ben_brackenbury' label='𝕏' />
-          <SocialLink href='https://github.com/benbrackenbury' label='GitHub' icon={faGithub} / >
-          <SocialLink href='https://linkedin.com/in/benbrackenbury' label='LinkedIn' icon={faLinkedinIn} / >
-          <SocialLink href='https://apps.apple.com/bt/developer/ben-brackenbury/id1518789219' label='App Store' icon={faAppStore} / >
+        <div className="my-5 mb-20 flex select-none flex-row justify-center gap-7 text-4xl">
+          <SocialLink href="https://x.com/ben_brackenbury" label="𝕏" />
+          <SocialLink
+            href="https://github.com/benbrackenbury"
+            label="GitHub"
+            icon={faGithub}
+          />
+          <SocialLink
+            href="https://linkedin.com/in/benbrackenbury"
+            label="LinkedIn"
+            icon={faLinkedinIn}
+          />
+          <SocialLink
+            href="https://apps.apple.com/bt/developer/ben-brackenbury/id1518789219"
+            label="App Store"
+            icon={faAppStore}
+          />
         </div>
       </div>
     </main>
-  )
+  );
 }
-
 
 type SocialLinkProps = {
-  href: string
-  label: string
-  icon?: IconDefinition
-}
-const SocialLink = ({ href, icon=undefined, label }: SocialLinkProps) => (
-  <a 
+  href: string;
+  label: string;
+  icon?: IconDefinition;
+};
+const SocialLink = ({ href, icon = undefined, label }: SocialLinkProps) => (
+  <a
     href={href}
     target="_blank"
     aria-label={label}
-    className='opacity-50 hover:opacity-100 p-1 hover:text-accent focus:text-accent focus:  opacity-100'
+    className="focus: p-1 opacity-100 opacity-50  hover:opacity-100"
   >
-    { icon ? <FontAwesomeIcon icon={icon}/> : <span className="font-bold">{label}</span> }
+    {icon ? (
+      <FontAwesomeIcon icon={icon} />
+    ) : (
+      <span className="font-bold">{label}</span>
+    )}
   </a>
-)
+);
